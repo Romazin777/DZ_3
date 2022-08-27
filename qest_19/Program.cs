712b00b4aaab17fb9fedd72f20ac5ae3571ed12c
Denis Romazin <romazin777@gmail.com>
2022-08-27 15:24:44 +0500

    Задача №19

---
+++ qest_19/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача №19. Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+  
+string palindrom5()
+{
+    Console.WriteLine("Введите 5 значное число");
+            int numb = Convert.ToInt32(Console.ReadLine());
+            string result = string.Empty;
+            if (numb / 100000 >= 1) result = ("Введенное число имеет Больше 5 знаков");
+            else if (numb / 10000 < 1) result = ("Введенное число имеет Меньше 5 знаков");
+            else if (numb % 10 == numb / 10000 % 10 && numb / 10 % 10 == numb / 1000 % 10) result = ("Введенное число Является палиндромом");
+            else result = ("Введенное число НЕ является палиндромом");
+            return result;
+}         
+try
+{
+    Console.WriteLine(palindrom5());
+}
+catch
+{
+    Console.WriteLine("Введите целое 5 значное число Цифрами");
+}
