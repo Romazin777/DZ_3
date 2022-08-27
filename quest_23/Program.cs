abbade505e9cef08586c31d9d28d1f758106de0e
Denis Romazin <romazin777@gmail.com>
2022-08-27 16:03:52 +0500

    Задача №23

---
+++ quest_23/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача №23 Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+string numbCUB()
+{
+    Console.Write("Введите положительное число: ");
+            int N = Convert.ToInt32(Console.ReadLine());
+            int i=1;
+            string res = string.Empty;
+            if (N < 1) res = "Нужно ввести число больше 1";
+            while (i <= N)
+            {
+                res = res + i*i*i +", ";
+                i++;
+            }
+    return res;
+}         
+try
+{
+    Console.WriteLine(numbCUB());
+}
+catch
+{
+    Console.WriteLine("Введите число ЦИФРАМИ");
+}
